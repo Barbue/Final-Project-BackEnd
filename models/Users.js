2425ae357456f3b8d9ea278577be1cb9d5766a6f
Barbue <david.crochet@codeimmersives.com>
2023-04-19 16:45:14 -0500

    added mongoose schema for users

--- models/Users.js
+++ models/Users.js
@@ -1,17 +1,13 @@
-//import mongoose library
 const mongoose = require("mongoose");
-const { v4: uuidv4 } = require("uuid");
+const { v4: uuid } = require("uuid");
 
-//create a userSchema 
 const userSchema = new mongoose.Schema({
-    email: String,
-    password: String, 
-    id: {type: String, default: uuidv4},
-}); 
+	email: { type: String, require: true, lowercase: true, unique: true },
+	hashedPassword: { type: String, require: true },
+	role: { type: String },
+	id: { type: String, default: uuid },
+});
 
-
-//register model to collection
 const User = mongoose.model("users", userSchema);
 
-//make our model accessible to outside files 
 module.exports = User;
